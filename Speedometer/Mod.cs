13bfb302b3eb101f804cf2cb8da5f3f9562c5a59
Ciastex <Ciastex@users.noreply.github.com>
2019-08-29 14:13:07 +0200

    Add some goodies.

--- Speedometer/Mod.cs
+++ Speedometer/Mod.cs
@@ -1,4 +1,5 @@
 ﻿using Reactor.API.Attributes;
+using Reactor.API.Configuration;
 using Reactor.API.Interfaces.Systems;
 using Reactor.API.Storage;
 using System;
@@ -6,19 +7,28 @@ using UnityEngine;
 
 namespace Speedometer
 {
-    [ModEntryPoint("com.github.ciastex/Speedometer")]
+    [ModEntryPoint(ModID)]
     public class Mod : MonoBehaviour
     {
+        private const string ModID = "com.github.ciastex/Speedometer";
+
+        internal const string ShowMeterSettingsKey = "ShowMeter";
+        internal const string MarginXSettingsKey = "MarginX";
+        internal const string MarginYSettingsKey = "MarginY";
+        internal const string CornerSettingsKey = "Corner";
+
         private Font _font;
         private GUIStyle _style;
+        private readonly AssetBundle _bundle = new Assets("speedometer.bndl").Bundle;
 
-        private float _margin = 16;
-
-        private AssetBundle _bundle = new Assets("speedometer.bndl").Bundle;
+        internal static Settings Settings;
 
         public void Initialize(IManager manager)
         {
+            SetUpSettings();
+
             _font = _bundle.LoadAsset<Font>("sunspire");
+
             _style = new GUIStyle()
             {
                 fontStyle = FontStyle.Normal,
@@ -29,10 +39,33 @@ namespace Speedometer
                 font = _font,
                 fontSize = 36
             };
+
+            CommandTerminal.Terminal.Shell.AddCommand(
+                "speedometer.toggle",
+                ShellCommands.ToggleSpeedometer,
+                0, 0,
+                "Toggles speedometer on/off."
+            );
+
+            CommandTerminal.Terminal.Shell.AddCommand(
+                "speedometer.corner",
+                ShellCommands.SetCorner,
+                1, 1,
+                "Set preferred display corner (TL/TR/BL/BR)."
+            );
+            CommandTerminal.Terminal.Shell.AddCommand(
+                "speedometer.margins",
+                ShellCommands.Margins,
+                2, 2,
+                "Set spacing between display corner and speedometer text."
+            );
         }
 
         public void OnGUI()
         {
+            if (!Settings.GetItem<bool>(ShowMeterSettingsKey))
+                return;
+
             var comp = FindObjectOfType<GTTODManager>();
 
             if (comp)
@@ -49,17 +82,51 @@ namespace Speedometer
                     var content = new GUIContent(Math.Round(speed, 2).ToString());
                     var dims = _style.CalcSize(content);
 
-                    var targetX = 0 + _margin;
-                    var targetY = Screen.height - dims.y - _margin;
+                    float targetX, targetY;
+                    switch (Settings.GetItem<string>(CornerSettingsKey))
+                    {
+                        case "TL":
+                            targetX = Settings.GetItem<int>(MarginXSettingsKey);
+                            targetY = Settings.GetItem<int>(MarginYSettingsKey);
+                            break;
+
+                        case "TR":
+                            targetX = Screen.width - dims.x - Settings.GetItem<int>(MarginXSettingsKey);
+                            targetY = Settings.GetItem<int>(MarginYSettingsKey);
+                            break;
+
+                        case "BR":
+                            targetX = Screen.width - dims.x - Settings.GetItem<int>(MarginXSettingsKey);
+                            targetY = Screen.height - dims.y - Settings.GetItem<int>(MarginYSettingsKey);
+                            break;
+
+                        case "BL":
+                        default:
+                            targetX = Settings.GetItem<int>(MarginXSettingsKey);
+                            targetY = Screen.height - dims.y - Settings.GetItem<int>(MarginYSettingsKey);
+                            break;
+                    }
 
                     GUI.Label(
                         new Rect(targetX, targetY, dims.x, dims.y),
-                        //new Rect(100, 100, dims.x, dims.y),
                         content,
                         _style
                     );
                 }
             }
+        }
+
+        private void SetUpSettings()
+        {
+            Settings = new Settings("hud_settings");
+
+            Settings.GetOrCreate(ShowMeterSettingsKey, true);
+            Settings.GetOrCreate(MarginXSettingsKey, 16);
+            Settings.GetOrCreate(MarginYSettingsKey, 16);
+            Settings.GetOrCreate(CornerSettingsKey, "BL");
+
+            if (Settings.Dirty)
+                Settings.Save();
         }
     }
 }
---
+++ Speedometer/ShellCommands.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace Speedometer
+{
+    public class ShellCommands
+    {
+        private static readonly List<string> _corners = new List<string> { "BL", "BR", "TL", "TR" };
+
+        public static void ToggleSpeedometer(CommandTerminal.CommandArg[] args)
+        {
+            Mod.Settings[Mod.ShowMeterSettingsKey] = !Mod.Settings.GetItem<bool>(Mod.ShowMeterSettingsKey);
+            Mod.Settings.Save();
+        }
+
+        public static void SetCorner(CommandTerminal.CommandArg[] args)
+        {
+            var corner = args[0].String;
+
+            if (!_corners.Contains(corner))
+            {
+                CommandTerminal.Terminal.Log("Valid corner descriptors are BL, BR, TL and TR");
+                return;
+            }
+
+            Mod.Settings[Mod.CornerSettingsKey] = corner;
+            Mod.Settings.Save();
+        }
+
+        public static void Margins(CommandTerminal.CommandArg[] args)
+        {
+            var x = args[0].Float;
+            var y = args[1].Float;
+
+            Mod.Settings[Mod.MarginXSettingsKey] = x;
+            Mod.Settings[Mod.MarginYSettingsKey] = y;
+
+            Mod.Settings.Save();
+        }
+    }
+}
